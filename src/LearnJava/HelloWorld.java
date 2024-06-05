ad92882d7f420ff73f255747b318e0421fcae75a
Nguyen <nguyentkhd2023@gmail.com>
2024-06-05 15:17:12 +0700

    Source code

---
+++ src/LearnJava/HelloWorld.java
@@ -0,0 +1,11 @@
+package LearnJava;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+	}
+}
